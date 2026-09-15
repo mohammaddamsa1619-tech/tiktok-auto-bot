c8115053acc73d65e0b514ccbb7c8fd1a3983621
mohammaddamsa1619-tech <mohammaddamsa1619@gmail.com>
2026-09-15 08:32:54 +0300

    Delete netlify/functions directory

--- netlify/functions/netlify/functions/scheduled-post.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async () => {
-  console.log("🚀 نشر تلقائي كل 24 ساعة");
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ ok: true, time: new Date().toISOString() })
-  };
-};
